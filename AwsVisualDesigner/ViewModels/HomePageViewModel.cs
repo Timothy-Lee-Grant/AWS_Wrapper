553babf66cd789d4f076322038f8caa7d4b564a9
Timothy-Lee-Grant <timothyleegrant1995@gmail.com>
2026-01-18 15:27:25 -0800

    Adjusting GUI to incorporate a drop down menu

--- AwsVisualDesigner/ViewModels/HomePageViewModel.cs
+++ AwsVisualDesigner/ViewModels/HomePageViewModel.cs
@@ -11,4 +11,11 @@ public partial class HomePageViewModel : ViewModelBase
 {
     public string Message {get;} = "This is my message";
     [ObservableProperty] private string _privateMessage = "This is my private message";
+    public List<string> Regions {get;} = new() {"east-1", "east-2", "west-1"};
+    [ObservableProperty] private string? _selectedRegion;
+
+    partial void OnSelectedRegionChanged(string? item)
+    {
+        Console.WriteLine($"I just got the data and you selected {item}");
+    }
 }
